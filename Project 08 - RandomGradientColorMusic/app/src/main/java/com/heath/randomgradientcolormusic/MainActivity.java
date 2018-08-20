5755b875b29eaffd2c9703ff65281e84139c2bba
Heath <heathhsia@gmail.com>
2018-08-20 15:16:49 +0800

    add GradientView

--- Project 08 - RandomGradientColorMusic/app/src/main/java/com/heath/randomgradientcolormusic/MainActivity.java
+++ Project 08 - RandomGradientColorMusic/app/src/main/java/com/heath/randomgradientcolormusic/MainActivity.java
@@ -2,12 +2,28 @@ package com.heath.randomgradientcolormusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyView myView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myView = (MyView) findViewById(R.id.my_view);
+
+    }
+
+    /**
+     * BackView 点击方法
+     * @param view
+     */
+    public void myViewClick(View view) {
+        Log.d("click", "view");
+        myView.colors = new int[]{getResources().getColor(R.color.colorGreen), getResources().getColor(R.color.colorRed), getResources().getColor(R.color.colorBlue)};
+        myView.invalidate();
     }
 }
---
+++ Project 08 - RandomGradientColorMusic/app/src/main/java/com/heath/randomgradientcolormusic/MyView.java
@@ -0,0 +1,39 @@
+package com.heath.randomgradientcolormusic;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyView extends View {
+
+    public int[] colors = new int[]{getResources().getColor(R.color.colorRed), getResources().getColor(R.color.colorBlue), getResources().getColor(R.color.colorGreen)};
+
+    public MyView (Context context) {
+        super(context);
+    }
+
+    public MyView (Context context, @Nullable AttributeSet attr) {
+        super(context, attr);
+    }
+
+    public MyView (Context context, @Nullable AttributeSet attr, int defStyleAttr) {
+        super(context, attr, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int width = getWidth();
+        int height = getHeight();
+        Paint paint = new Paint();
+        LinearGradient backGradient = new LinearGradient(0, 0, width, height, colors, null, Shader.TileMode.CLAMP);
+        paint.setShader(backGradient);
+        canvas.drawRect(0,0, width, height, paint);
+    }
+
+}
